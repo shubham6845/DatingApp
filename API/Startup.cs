6df72bc819e599d9762dd563c161e6152721cefd
shubham6845 <sgodhani@aakashinformatics.com>
2022-01-26 11:54:43 +0530

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,8 @@ namespace API
               options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -51,6 +53,7 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
